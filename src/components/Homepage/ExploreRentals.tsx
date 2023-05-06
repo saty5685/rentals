a1e92c8a7b00aa8124ac4f13a62d9bfb785e9709
Satyam  Singh <satyam@turings.xyz>
2023-05-07 01:33:38 +0530

    bg image added in homepage with explore button

---
+++ src/components/Homepage/ExploreRentals.tsx
@@ -0,0 +1,18 @@
+
+import '../../css/ExploreRentals.css'
+export const ExploreRentals = () => {
+    return (
+
+        <div className=' p-5 explore-bg-image header explore-content'>
+            <div className='py-5 explore-content-text'>
+                <h1 className='display-5 fw-bold'>
+                   Find your next adventure <br />
+                </h1>
+               <p>Where would you like to go nex?</p>
+               <a type='button' className='btn main-color btn-lg text-white' href='#'>Explore top books</a>
+            </div>
+
+        </div>
+
+    );
+}
