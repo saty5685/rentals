3754d9cb51dbb8adf7b5175e28b5d8c5a9df3068
Satyam  Singh <satyam@turings.xyz>
2023-05-13 01:47:40 +0530

    Heros added in homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Navbar } from './components/Navbar';
 import { ExploreRentals } from './components/Homepage/ExploreRentals';
 import { Carousel } from './components/Homepage/Carousel';
+import { Heros } from './components/Homepage/Heros';
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreRentals />
       <Carousel/>
+      <Heros/>
     </div>
 
   );
